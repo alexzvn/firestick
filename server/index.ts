40c524e54423e652314b998e6ab89d445ca8ea9b
Alexzvn <personal@alexzvn.me>
2024-08-21 18:25:10 +0700

    wip

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,5 @@
 import { application } from './src/application/index.ts'
-import type { Prettify } from '~/application/Application.ts'
 import Router from '~/http/router'
 
-export const elysia = application.start(void 0)
-  .then(_app => _app.service.elysia.use(Router))
-
-export type ElysiaServer = Prettify<Awaited<typeof elysia>>
+application.start(void 0)
+  .then(app => app.service.elysia.use(Router))
--- server/src/http/router/web.router.ts
+++ server/src/http/router/web.router.ts
@@ -2,5 +2,5 @@ import Elysia from 'elysia'
 import Auth from '../controller/auth/Auth'
 
 export default new Elysia()
-  .use(Auth)
+  .group('/web', app => app.use(Auth))
   
